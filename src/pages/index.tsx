e8e439ab6faf961b99743932f2c040f88cf65e97
shuta13 <hollystarsun@gmail.com>
2021-03-24 23:03:51 +0900

    wip

---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+import { graphql } from "gatsby"
+
+const BlogIndex: React.FC = () => {
+  return (
+    <>
+      <></>
+    </>
+  )
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+        }
+      }
+    }
+  }
+`
